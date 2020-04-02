e970fc94c1742042512f7db6cd849a19d9d79b92
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-04-01 20:18:15 -0500

    Decorator ok

--- decorator.py
+++ decorator.py
@@ -1,5 +1,20 @@
+PASSWORD = '12345'
+
+
+def password_required(func):
+    def wrapper():
+        password = input('What is your password? ')
+
+        if password == PASSWORD:
+            return func()
+        else:
+            print('Invalid password')
+    
+    return wrapper
+
+@password_required
 def needs_password():
-    print('Contrasena correcta')
+    print('Correct Password')
 
 
 if __name__ == '__main__':
